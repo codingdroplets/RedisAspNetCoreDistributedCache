9bbb904150922902f9e6be28bd6651044b50ab1d
Coding Droplets <93096161+codingdroplets@users.noreply.github.com>
2021-11-10 17:10:29 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using RedisDemo.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace RedisDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IDistributedCache _distributedCache;
+
+        public HomeController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        public async Task<ActionResult> SaveRedisCache()
+        {
+            var dashboadData = new DashboardData
+            {
+                TotalCustomerCount = 100450,
+                TotalRevenue = 12908092,
+                TopSellingCountryName = "United States",
+                TopSellingProductName = "Macbook Pro"
+            };
+
+            var tomorrow = DateTime.Now.Date.AddDays(1);
+            var totalSeconds = tomorrow.Subtract(DateTime.Now).TotalSeconds;
+
+            var distributedCacheEntryOptions = new DistributedCacheEntryOptions();
+            distributedCacheEntryOptions.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(totalSeconds);
+            distributedCacheEntryOptions.SlidingExpiration = null;
+
+            var jsonData = JsonConvert.SerializeObject(dashboadData);
+            await _distributedCache.SetStringAsync("DashboardData", jsonData, distributedCacheEntryOptions);
+
+            return View();
+        }
+
+        public async Task<ActionResult> Dashboard()
+        {
+            var jsonData = await _distributedCache.GetStringAsync("DashboardData");
+            var dashboardData = JsonConvert.DeserializeObject<DashboardData>(jsonData);
+            ViewBag.DashboardData = dashboardData;
+            return View();
+        }
+    }
+}
---
+++ Models/DashboardData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RedisDemo.Models
+{
+    [Serializable]
+    public class DashboardData
+    {
+        public int TotalCustomerCount { get; set; }
+
+        public int TotalRevenue { get; set; }
+
+        public string TopSellingProductName { get; set; }
+
+        public string TopSellingCountryName { get; set; }
+    }
+}
